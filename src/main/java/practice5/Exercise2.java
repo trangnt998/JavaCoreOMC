8235056968f39b32bb75ef44c0d453e217f53ce7
Trang Nguyễn <trangnt998@gmail.com>
2021-08-11 16:29:23 +0700

    * solved practice 5 (Exercises: 2,3)

---
+++ src/main/java/practice5/Exercise2.java
@@ -0,0 +1,109 @@
+package practice5;
+
+import java.util.Scanner;
+
+public class Exercise2 {
+    /*
+    2. Các thao tác kiểm tra
+        a. Mảng có phải là mảng toàn chẵn
+        b. Mảng có phải là mảng toàn số nguyên tố
+        c. Mảng có phải là mảng tăng dần
+     */
+
+    public static int[] inputIntArray(int n) { //nhập mảng gồm n số nguyên
+        int[] intArr = new int[n];
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < n; i++) {
+            intArr[i] = sc.nextInt();
+        }
+        return intArr;
+    }
+
+    public static boolean isAllEven(int[] intArr) { // kiểm tra mảng toàn số chẵn
+        for (int j : intArr) {
+            if (j % 2 != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //check 1 số có phải nguyên tố không
+    public static boolean isPrime(int m) {
+        if (m == 2) {
+            return true;
+        } else if (m > 2) { //1 khong phai so nguyen to, 2 la so nguyen to...
+            for (int i = 2; i < m; i++) {
+                if (m % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    //kiểm tra mảng toàn số nguyên tố
+    public static boolean isAllPrime(int[] intArr) {
+        for (int i : intArr) {
+            if (!isPrime(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //kiểm tra mảng tăng dần
+    public static boolean isAscArr(int[] intArr) {
+        for (int i = 0; i < intArr.length - 1; i++) {
+            if (intArr[i] > intArr[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+        //test
+        //a. Mảng có phải là mảng toàn chẵn
+        System.out.println("===Kiểm tra mảng toàn số chẵn.");
+        System.out.print("Kích thước mảng: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if (n > 0) {
+            int[] intArr = inputIntArray(n);
+            boolean checkEvenArr = isAllEven(intArr);
+            System.out.println("Mảng toàn số chẵn? -> " + checkEvenArr);
+        } else {
+            System.out.println("Sai du lieu!");
+        }
+
+        //b. Mảng có phải là mảng toàn số nguyên tố
+        System.out.println("===Kiểm tra mảng toàn số nguyên tố.");
+        System.out.print("Kích thước mảng: ");
+        n = sc.nextInt();
+        if (n > 0) {
+            int[] intArr = inputIntArray(n);
+            boolean checkPrimeArr = isAllPrime(intArr);
+            System.out.println("Mang toan so nguyen to? -> " + checkPrimeArr);
+        } else {
+            System.out.println("Sai du lieu!");
+        }
+
+        //c. Mảng có phải là mảng tăng dần
+        System.out.println("===Kiểm tra mảng tăng dần.");
+        System.out.print("Kích thước mảng: ");
+        n = sc.nextInt();
+        if (n > 0) {
+            int[] intArr = inputIntArray(n);
+            boolean checkAscArr = isAscArr(intArr);
+            System.out.println("Mang tang dan? -> " + checkAscArr);
+        } else {
+            System.out.println("Sai du lieu!");
+        }
+
+    }
+}
---
+++ src/main/java/practice5/Exercise3.java
@@ -0,0 +1,67 @@
+package practice5;
+
+import java.util.Scanner;
+
+import static practice5.Exercise2.inputIntArray;
+import static practice5.Exercise2.isPrime;
+
+public class Exercise3 {
+    /*
+    3. Các thao tác tính toán
+        a. Có bao nhiêu số chia hết cho 4 nhưng không chia hết cho 5
+        b. Tổng các số nguyên tố có trong mảng
+     */
+
+    //Có bao nhiêu số chia hết cho 4 nhưng không chia hết cho 5
+    public static int div4Not5(int[] intArr) {
+        int count = 0;
+        for (int i : intArr) {
+            if ((i % 4 == 0) && (i % 5 != 0)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+    //b. Tổng các số nguyên tố có trong mảng
+    public static int sumPrimeArr(int[] intArr) {
+        int sumPrime = 0;
+        for (int i : intArr) {
+            if (isPrime(i)) {
+                sumPrime += i;
+            }
+        }
+        return sumPrime;
+    }
+
+    public static void main(String[] args) {
+        //test
+        //a. Có bao nhiêu số chia hết cho 4 nhưng không chia hết cho 5
+        System.out.println("===Kiểm tra mảng có bao nhiêu số chia hết cho 4 nhưng không chia hết cho 5.");
+        System.out.print("Kích thước mảng: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if (n > 0) {
+            int[] intArr = inputIntArray(n);
+            int count = div4Not5(intArr);
+            System.out.println(" -> " +count + " số");
+        } else {
+            System.out.println("Sai du lieu!");
+        }
+
+        //3b. Tổng các số nguyên tố có trong mảng
+        System.out.println("===Tính tổng các số nguyên tố có trong mảng.");
+        System.out.print("Kích thước mảng: ");
+        n = sc.nextInt();
+        if (n > 0) {
+            int[] intArr = inputIntArray(n);
+            int sumPrime = sumPrimeArr(intArr);
+            System.out.println("-> Tổng các số nguyên tố có trong mảng la: " + sumPrime);
+        } else {
+            System.out.println("Sai du lieu!");
+        }
+
+    }
+}
